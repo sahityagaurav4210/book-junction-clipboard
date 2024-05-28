4bf6a57f1d63c9f9a7b3971c5ec2573c923e5de8
sahityagaurav4210 <sahityagaurav.41125@gmail.com>
2024-05-28 12:56:36 +0530

    fix: cd pipeline
    fixed cd pipeline to run only on release branch and increased timeout of test cases

--- build/tests/clipboard.test.js
+++ build/tests/clipboard.test.js
@@ -7,11 +7,11 @@ describe("clipboard.test.js", function () {
         expect(typeof data).toBe("object");
         expect(typeof data.message).toBe("string");
         expect(typeof data.cerr === "string" || typeof data.cerr === "undefined").toBe(true);
-    });
+    }, 60000);
     it("copy function should return valid values", async function () {
         const data = await new index_1.default().copy(`git commit -m "hello" -m "world"`);
         expect(typeof data).toBe("object");
         expect(data.message).toBe("Text copied\n");
         expect(data.cerr).toBe("");
-    });
+    }, 60000);
 });
